b9d44b76022bb966424134006d79a8ca1a4b4920
Jennie <jennie@Jennies-MacBook-Pro.local>
2018-03-19 22:38:27 +0000

    ready for demo

--- src/components/slider.js
+++ src/components/slider.js
@@ -52,24 +52,25 @@ export class Slider extends Component{
         });
     }
 
-    // handleClick(direction) {
-    //     const slideWidth = $(this.state.parent + ' #slider ul li').width();   
-    //     var _this = this;
-    //     var childElement = direction == 'right' ? 'first-child' : 'last-child';
+    handleClick(direction) {
+        const slideWidth = $(this.state.parent + ' #slider ul li').width();   
+        var _this = this;
+        var childElement = direction == 'right' ? 'first-child' : 'last-child';
         
-    //     $(_this.state.parent + ' #slider ul').animate({
-    //         left: - slideWidth
-    //     }, 200, function () {
-    //         $(_this.state.parent + ' #slider ul li:' + childElement).appendTo(_this.state.parent + ' #slider ul');
-    //         $(_this.state.parent + ' #slider ul').css('left', '');
-    //     });
-    // }
+        $(_this.state.parent + ' #slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $(_this.state.parent + ' #slider ul li:' + childElement).appendTo(_this.state.parent + ' #slider ul');
+            $(_this.state.parent + ' #slider ul').css('left', '');
+        });
+    }
 
     render(){
         return (
             <div className={"slider-container slider-container-"+this.props.type}>
                 <h1>{this.props.gender} Shoes</h1>
                 <div id="slider">
+                    {/* <a className="control_next" onClick={()=>this.handleClick('right')}> &gt; </a> */}
                     <a className="control_next" onClick={this.handleRightClick}> &gt; </a>
                     <a className="control_prev" onClick={this.handleLeftClick}> &lt; </a>
                     <Categories type={this.props.gender} gender={this.props.gender} image={true}/>
